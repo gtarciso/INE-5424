fd58ec574780088c01dd8c290fa485a884ff805a
Gustavo Tarciso <gustavotarciso@hotmail.com>
2020-11-01 13:47:00 -0300

    versão base disponibilizada para a segunda entrega

--- include/machine/riscv/sifive_e/sifive_e_memory_map.h
+++ include/machine/riscv/sifive_e/sifive_e_memory_map.h
@@ -13,14 +13,14 @@ struct Memory_Map
 {
     // Physical Memory
     enum {
-        UART_BASE                   = 0x10000000, // UART
-        TIMER_BASE                  = XXXXXXX,
-        GPIOA_BASE                  = XXXXXXX,
-        RTC_BASE                    = 0x101000,
+        UART_BASE                   = 0x10000000, // 16550A NS UART
+        TIMER_BASE                  = 0x00000000, // IMPLEMENT 
+        GPIOA_BASE                  = 0x03000000, // gpex_ioport
+        RTC_BASE                    = 0x00101000, // goldfish_rtc
 
-        CLINT_BASE                  = 0x2000000,
-        PLIIC_CPU_BASE              = 0xc000000,
-        PRIVATE_TIMER_BASE          = XXXXXXX
+        CLINT_BASE                  = 0x00000000, // IMPLEMENT (Sifive CLINT)
+        PLIC_CPU_BASE               = 0x0c000000, // Sifive PLIC
+        PRIVATE_TIMER_BASE          = 0x00000000  // IMPLEMENT (mtime_base)
     };
 
     // Physical Memory
