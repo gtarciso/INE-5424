ba98ef00d9b6a42c2e04ab4935303e894d749bac
Gustavo Tarciso <gustavotarciso@hotmail.com>
2020-11-10 22:08:55 -0300

    alinhando os tabs

--- src/setup/setup_sifive_e.cc
+++ src/setup/setup_sifive_e.cc
@@ -2,7 +2,10 @@
 
 #include <system/config.h>
 
-extern "C" { void _vector_table() __attribute__ ((used, naked, section(".init"))); }
+extern "C"
+{
+    void _vector_table() __attribute__((used, naked, section(".init")));
+}
 
 // Interrupt Vector Table
 void _vector_table()
