a039e457122b268afcbf06a0393e9147c1f10819
Gustavo Tarciso <gustavotarciso@hotmail.com>
2020-11-11 20:13:55 -0300

    operações atomicas implementadas, necessita de testes

--- include/architecture/rv32/rv32_cpu.h
+++ include/architecture/rv32/rv32_cpu.h
@@ -179,6 +179,44 @@ public:
 
     using CPU_Common::cas; // IMPLEMENT
 
+    template<typename T>
+    static T finc(volatile T & value) {
+        register T old;
+        register T one = 1;
+        ASM(" amoadd.w %0, %1, (%2)   \n" : "=r"(old): "r"(one), "r"(value) : "t0", "cc");
+        return old;
+    }
+
+    template<typename T>
+    static T fdec(volatile T & value) {
+        register T old;
+        register T one = -1;
+        ASM(" amoadd.w %0, %1, (%2)   \n" : "=r"(old): "r"(one), "r"(value) : "t0", "cc");
+        return old;
+    }
+
+    template<typename T>
+    static T tsl(volatile T & lock) {
+        register T old;
+        register T one = 1;
+        ASM("1: lr.w %0, (%1)       \n"
+            "   sc.w t0, %2, (%1)   \n"
+            "   bnez t0, 1b         \n" : "=&r"(old) : "r"(lock), "r"(one) : "t0", "cc");
+        return old;
+    }
+
+    template <typename T>
+    static T cas(volatile T & value, T compare, T replacement) {
+        register T old;
+        ASM("1: lr.w    %0, (%1)        \n"
+            "   bne     %0, %2, f2      \n"
+            "   sc.w    t0, %3, (%1)    \n"
+            "   bnez    t0, 1b          \n"
+            "2:                         \n" : "=&r"(old) : "r"(value), "r"(compare), "r"(replacement) : "t0", "cc");
+        return old;
+    }
+
+
     // Power modes
     static void halt() { ASM("wfi"); }
 
