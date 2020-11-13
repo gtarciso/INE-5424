7610da04c08f1d322b329c727d6875a09c1f2ed3
Teo <teogallarza@gmail.com.br>
2020-11-12 23:34:46 -0300

    agora com uns panic a mais

--- src/api/thread_init.cc
+++ src/api/thread_init.cc
@@ -11,15 +11,20 @@ extern "C" { void __epos_app_entry(); }
 
 void Thread::init()
 {
+    
     db<Init, Thread>(TRC) << "Thread::init()" << endl;
 
-    // If EPOS is a library, then adjust the application entry point to __epos_app_entry,
-    // which will directly call main(). In this case, _init will have already been called,
-    // before Init_Application to construct MAIN's global objects.
-    new (SYSTEM) Thread(Thread::Configuration(Thread::RUNNING, Thread::MAIN), reinterpret_cast<int (*)()>(__epos_app_entry));
+    if(CPU::id() == 0){
 
+        // If EPOS is a library, then adjust the application entry point to __epos_app_entry,
+        // which will directly call main(). In this case, _init will have already been called,
+        // before Init_Application to construct MAIN's global objects.
+        new (SYSTEM) Thread(Thread::Configuration(Thread::RUNNING, Thread::MAIN), reinterpret_cast<int (*)()>(__epos_app_entry));
+    }
     // Idle thread creation does not cause rescheduling (see Thread::constructor_epilogue)
     new (SYSTEM) Thread(Thread::Configuration(Thread::READY, Thread::IDLE), &Thread::idle);
+    
+    CPU::smp_barrier();
 
     // The installation of the scheduler timer handler does not need to be done after the
     // creation of threads, since the constructor won't call reschedule() which won't call
