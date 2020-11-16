adea755b041d10465fe5e4f42c824d5d9250bf4d
Gustavo Tarciso <gustavotarciso@hotmail.com>
2020-11-16 15:32:51 -0300

    teste de operações atomicas e traits arrumados

--- app/c2/c2.cc
+++ app/c2/c2.cc
@@ -1,17 +1,25 @@
 #include <utility/ostream.h>
 #include <process.h>
+#include <synchronizer.h>
+
+#define TOTAL 45000
 
 using namespace EPOS;
 
 
 OStream cout;
 char buffer[2];
+Mutex control;
+
+int count;
 
 int func(int n)
 {
     for(int i = 0; i < 5000; i++){
-        cout << n << " cpu id:" << CPU::id() << endl;
-        Thread::yield();
+        control.lock();
+        cout << "computando no hart: " << CPU::id() << endl;
+        count += n;
+        control.unlock();
     }
     return 0;
 }
@@ -19,17 +27,21 @@ int func(int n)
 int main()
 {
     cout << "----------------------------------" << endl;
-    cout << "       TESTES CRITÉRIO 3 - INÍCIO " << endl;
+    cout << "       TESTES CRITÉRIO 2 - INÍCIO " << endl;
     cout << "----------------------------------" << endl;
 
+    count = 0;
 
-    cout << "Context Switch" << endl;
+    cout << "Somatorio multicore" << endl;
     Thread * a_thread = new Thread(Thread::Configuration(Thread::READY, Thread::Criterion(10000000)), &func, 1);
     cout << "Computando coisas em:" << a_thread << endl;
     Thread * b_thread = new Thread(Thread::Configuration(Thread::READY, Thread::Criterion(50000000)), &func, 5);
     cout << "Computando coisas em:" << b_thread << endl;
     Thread * c_thread = new Thread(Thread::Configuration(Thread::READY, Thread::Criterion(30000000)), &func, 3);
     cout << "Computando coisas em:" << c_thread << endl;
+
+    cout << "verificando se a contagem foi correta" << endl;
+    assert(count == TOTAL);
 
     a_thread->join();
     cout << "Joining thread:" << a_thread <<endl;
@@ -45,7 +57,7 @@ int main()
     delete c_thread;
 
     cout << "----------------------------------" << endl;
-    cout << "       TESTES CRITÉRIO 3 - FIM    " << endl;
+    cout << "       TESTES CRITÉRIO 2 - FIM    " << endl;
     cout << "----------------------------------" << endl;
 
     return 0;
