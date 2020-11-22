2939f2e6ee55530c4e3e5ced0d512769e1165e60
Gustavo Tarciso <gustavotarciso@hotmail.com>
2020-11-22 10:10:30 -0300

    teste c4 implementado

--- app/c4/c4.cc
+++ app/c4/c4.cc
@@ -9,42 +9,9 @@ char buffer[2];
 
 int func(int n)
 {
-    bool enable_0 = true;
-    bool enable_1 = true;
-    bool enable_2 = true;
-    bool enable_3 = true;
-    for(;;)
-    {
-        int j = 5; 
-        if (CPU::id() == 0 && enable_0)
-        {
-            cout << "0 ";
-            enable_0 = false;
-            CPU::smp_barrier();
-            return 0;
-        }
-        if (CPU::id() == 1 && enable_1)
-        {
-            cout << "1 ";
-            enable_1 = false;
-            CPU::smp_barrier();
-            return 0;
-        }
-        if (CPU::id() == 2 && enable_2)
-        {
-            cout << "2 ";
-            enable_2 = false;
-            CPU::smp_barrier();
-            return 0;
-        }
-        if (CPU::id() == 3 && enable_3)
-        {
-            cout << "3 ";
-            enable_3 = false;
-            CPU::smp_barrier();
-            return 0;
-        }
-        j = j*j;  // uma operacao para consumir clocks
+    for(int i = 0; i < 5; i++){
+        cout << n << endl;
+        Thread::yield();
     }
     return 0;
 }
@@ -52,36 +19,41 @@ int func(int n)
 int main()
 {
     cout << "----------------------------------" << endl;
-    cout << "       TESTES CRITÉRIO 3 - INÍCIO " << endl;
+    cout << "       TESTES CRITÉRIO 4 - INÍCIO " << endl;
     cout << "----------------------------------" << endl;
 
 
-    cout << "Context Switch" << endl;
+    cout << "Creating Threads" << endl;
     Thread * a_thread = new Thread(Thread::Configuration(Thread::READY, Thread::Criterion(10000000)), &func, 1);
-    Thread * b_thread = new Thread(Thread::Configuration(Thread::READY, Thread::Criterion(50000000)), &func, 5);
-    Thread * c_thread = new Thread(Thread::Configuration(Thread::READY, Thread::Criterion(30000000)), &func, 3);
-    Thread * d_thread = new Thread(Thread::Configuration(Thread::READY, Thread::Criterion(20000000)), &func, 2);
-    Thread * e_thread = new Thread(Thread::Configuration(Thread::READY, Thread::Criterion(10000000)), &func, 1);
-    Thread * f_thread = new Thread(Thread::Configuration(Thread::READY, Thread::Criterion(50000000)), &func, 5);
-    Thread * g_thread = new Thread(Thread::Configuration(Thread::READY, Thread::Criterion(30000000)), &func, 3);
-    Thread * h_thread = new Thread(Thread::Configuration(Thread::READY, Thread::Criterion(20000000)), &func, 2);
+    Thread * b_thread = new Thread(Thread::Configuration(Thread::READY, Thread::Criterion(10000000)), &func, 1);
+    Thread * c_thread = new Thread(Thread::Configuration(Thread::READY, Thread::Criterion(10000000)), &func, 1);
+    Thread * d_thread = new Thread(Thread::Configuration(Thread::READY, Thread::Criterion(10000000)), &func, 1);
 
-    a_thread->join();
-    cout << "Joining thread:" << a_thread <<endl;
+    Thread * e_thread = new Thread(Thread::Configuration(Thread::READY, Thread::Criterion(50000000)), &func, 5);
+    Thread * f_thread = new Thread(Thread::Configuration(Thread::READY, Thread::Criterion(50000000)), &func, 5);
+    Thread * g_thread = new Thread(Thread::Configuration(Thread::READY, Thread::Criterion(50000000)), &func, 5);
+    Thread * h_thread = new Thread(Thread::Configuration(Thread::READY, Thread::Criterion(50000000)), &func, 5);
+    
+    Thread * i_thread = new Thread(Thread::Configuration(Thread::READY, Thread::Criterion(30000000)), &func, 3);
+    Thread * j_thread = new Thread(Thread::Configuration(Thread::READY, Thread::Criterion(30000000)), &func, 3);
+    Thread * k_thread = new Thread(Thread::Configuration(Thread::READY, Thread::Criterion(30000000)), &func, 3);
+    Thread * l_thread = new Thread(Thread::Configuration(Thread::READY, Thread::Criterion(30000000)), &func, 3);
+    
+    cout << "Joining threads" << a_thread <<endl;
+    a_thread->join();    
     b_thread->join();
-    cout << "Joining thread:" << b_thread <<endl;
     c_thread->join();
-    cout << "Joining thread:" << c_thread <<endl;
     d_thread->join();
-    cout << "Joining thread:" << d_thread <<endl;
+
     e_thread->join();
-    cout << "Joining thread:" << e_thread <<endl;
     f_thread->join();
-    cout << "Joining thread:" << f_thread <<endl;
     g_thread->join();
-    cout << "Joining thread:" << g_thread <<endl;
     h_thread->join();
-    cout << "Joining thread:" << h_thread <<endl;
+
+    i_thread->join();
+    j_thread->join();
+    k_thread->join();
+    l_thread->join();
 
     cout << "The end!" << endl;
 
@@ -89,13 +61,19 @@ int main()
     delete b_thread;
     delete c_thread;
     delete d_thread;
+
     delete e_thread;
     delete f_thread;
     delete g_thread;
     delete h_thread;
 
+    delete i_thread;
+    delete j_thread;
+    delete k_thread;
+    delete l_thread;
+
     cout << "----------------------------------" << endl;
-    cout << "       TESTES CRITÉRIO 3 - FIM    " << endl;
+    cout << "       TESTES CRITÉRIO 4 - FIM    " << endl;
     cout << "----------------------------------" << endl;
 
     return 0;
